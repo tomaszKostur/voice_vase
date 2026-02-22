f7cd85c37a9571c99173363ed83d4c3cd79187e2
Tomasz Kostur <tomasz.kostur@gmail.com>
2026-02-22 17:14:03 +0100

    Add tailwind

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use leptos::prelude::*;
+use leptos::{html::button, prelude::*};
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
     components::{Route, Router, Routes},
@@ -31,10 +31,10 @@ pub fn App() -> impl IntoView {
     view! {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
-        <Stylesheet id="leptos" href="/pkg/voice_base.css"/>
+        <Stylesheet id="baza_glosow" href="/pkg/voice_base.css"/>
 
         // sets the document title
-        <Title text="Welcome to Leptos"/>
+        <Title text="Baza Głosów"/>
 
         // content for this welcome page
         <Router>
@@ -51,11 +51,48 @@ pub fn App() -> impl IntoView {
 #[component]
 fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
-    let count = RwSignal::new(0);
-    let on_click = move |_| *count.write() += 1;
+    // let count = RwSignal::new(0);
+    // let on_click = move |_| *count.write() += 1;
 
+    // view! {
+    //     <h1>"Welcome to Leptos!"</h1>
+    //     <button on:click=on_click>"Click Me: " {count}</button>
+    // }
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <TitleBar/>
+        <MenuBar/>
+        <O_Nas/>
+    }
+}
+
+#[component]
+fn TitleBar() -> impl IntoView {
+    view! {
+        <h1>Baza Głosów</h1>
+    }
+}
+
+#[component]
+fn MenuBar() -> impl IntoView {
+    view! {
+        <div class="inline-flex rounded-base shadow-xs -space-x-px" role="group">
+            <button type="button" class="text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-3 focus:ring-neutral-tertiary-soft font-medium leading-5 rounded-s-base text-sm px-3 py-2 focus:outline-none">Szukaj</button>
+            <button type="button" class="text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-3 focus:ring-neutral-tertiary-soft font-medium leading-5 rounded-s-base text-sm px-3 py-2 focus:outline-none">Lista</button>
+            <button type="button" class="text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-3 focus:ring-neutral-tertiary-soft font-medium leading-5 rounded-s-base text-sm px-3 py-2 focus:outline-none">Nowi aktorzy</button>
+            <button type="button" class="text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-3 focus:ring-neutral-tertiary-soft font-medium leading-5 rounded-s-base text-sm px-3 py-2 focus:outline-none">O nas</button>
+            <button type="button" class="text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-3 focus:ring-neutral-tertiary-soft font-medium leading-5 rounded-s-base text-sm px-3 py-2 focus:outline-none">FAQ</button>
+        </div>
+    }
+}
+
+#[component]
+fn O_Nas() -> impl IntoView {
+    view! {
+        <h3>O Nas</h3>
+        <ul>
+            <li>Propagowanie artystów</li>
+            <li>Widoczność</li>
+            <li>Współpraca</li>
+        </ul>
     }
 }
