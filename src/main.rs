67621911d25239d470222829799cefdaf9f82064
Tomasz Kostur <tomasz.kostur@gmail.com>
2026-03-24 19:36:08 +0100

    Make sqlx dependency work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
 
+// #[cfg(feature = "ssr")]
+// pub mod ssr {
+//     // use http::{header::SET_COOKIE, HeaderMap, HeaderValue, StatusCode};
+//     use leptos::server_fn::ServerFnError;
+//     use sqlx::{Connection, SqliteConnection};
+
+//     pub async fn db() -> Result<SqliteConnection, ServerFnError> {
+//         Ok(SqliteConnection::connect("sqlite:Todos.db").await?)
+//     }
+// }
+
+
+
+
 #[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
@@ -7,12 +21,16 @@ async fn main() {
     use leptos::prelude::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
     use voice_base::app::*;
+    
 
     let conf = get_configuration(None).unwrap();
     let addr = conf.leptos_options.site_addr;
     let leptos_options = conf.leptos_options;
     // Generate the list of routes in your Leptos App
     let routes = generate_route_list(App);
+
+    let db_pool = sqlx::PgPool::connect("postgres://postgres:do_change_me_later_123/localhost/voidebase").await.expect("Cannot connect to DB");
+    // let mut conn = db().await.expect("couldn't connect to DB");
 
     let app = Router::new()
         .leptos_routes(&leptos_options, routes, {
