b5519e3761b82b88e936cf3dddaf1f4d66b8c8ef
Tomasz Kostur <tomasz.kostur@gmail.com>
2026-08-06 19:51:15 +0200

    Added tower cookies to project

--- src/axum_sandbox.rs
+++ src/axum_sandbox.rs
@@ -1,12 +1,18 @@
 use axum::{
     extract::Query,
     response::{Html, IntoResponse},
-    routing::get,
-    Router,
+    routing::{get, post},
+    Json, Router,
 };
 use leptos::config::LeptosOptions;
+use leptos_router::Method::Post;
 use serde::Deserialize;
+use serde_json::{json, Value};
+use tower_cookies::{Cookie, Cookies};
 
+pub use error::{Error, Result};
+
+//////////////////////////
 #[derive(Debug, Deserialize)]
 pub struct SandboxParams {
     name: Option<String>,
@@ -21,6 +27,55 @@ pub async fn hello_axum_sandbox(Query(params): Query<SandboxParams>) -> impl Int
     Html(format!("Axum sandbox hello <string>{name}</strong>"))
 }
 
+//////////////////////////////
+/// api login
+///
+
+#[derive(Debug, Deserialize)]
+struct LoginParams {
+    username: String,
+    password: String,
+}
+
+async fn api_login(payload: Json<LoginParams>) -> Result<Json<Value>> {
+    println!(
+        "->> {:<12} log from hello_axum_sandbox: params: {payload:?}",
+        "API LOGIN"
+    );
+    // credential check placeholder
+    if payload.username != "tomasz" || payload.password != "impossible" {
+        return Err(Error::LoginFail);
+    }
+
+    let body = Json(json!({"result":{"success": true}}));
+    Ok(body)
+}
+
+//////////////////////////
+
 pub fn get_axum_sandbox_router() -> Router<LeptosOptions> {
-    Router::new().route("/axum_sb", get(hello_axum_sandbox))
+    Router::new()
+        .route("/axum_sb", get(hello_axum_sandbox))
+        .route("/axum_sb/login", post(api_login))
+}
+
+mod error {
+    use axum::{
+        http::StatusCode,
+        response::{IntoResponse, Response},
+    };
+
+    pub type Result<T> = core::result::Result<T, Error>;
+
+    #[derive(Debug)]
+    pub enum Error {
+        LoginFail,
+    }
+
+    impl IntoResponse for Error {
+        fn into_response(self) -> Response {
+            println!("->> {:<12} - {self:?}", "INTO_RES");
+            (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        }
+    }
 }
--- tests/axum_sandbox_tests.rs
+++ tests/axum_sandbox_tests.rs
@@ -1,15 +1,24 @@
 #![allow(unused)] // Not for production
 #![cfg(feature = "ssr")]
 
-
+use serde_json::json;
 use anyhow::Result;
+
+// WARNING: This are dev tests. The tev tests need server to be setup manually
 
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:3000")?;
     hc.do_get("/axum_sb").await?.print().await?;
-
     hc.do_get("/axum_sb?name=Tomasz").await?.print().await?;
+    Ok(())
+}
 
+
+#[tokio::test]
+async fn login_cycle() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:3000")?;
+    hc.do_get("/axum_sb").await?.print().await?;
+    hc.do_post("/axum_sb/login", json!({"username": "tomas", "password": "impossible"})).await?.print().await?;
     Ok(())
 }
